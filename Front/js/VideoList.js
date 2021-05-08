76072e137f930a95c1e2e01494eaa16285ce76e5
ryeong <wofud0321@naver.com>
2021-05-08 23:56:43 +0900

    3

--- Front/js/VideoList.js
+++ Front/js/VideoList.js
@@ -15,14 +15,49 @@
 $.fn.divcreate = function(){
     for(var i=1; i<10; i++){
     var video = document.createElement("div"); 
-    video.id = 'video'+i;
+    video.id = 'video';
     video.style.width='350px';
     video.style.height='250px';
     video.style.float="left";
     video.style.margin="100px";
     video.style.backgroundColor="yellow";
+    video.style.cursor="pointer";
+    
+    
     $("#Contents").append(video);
+    var pop = document.createElement("div"); 
+    pop.id = "element_to_pop_up";
+    video.append(pop);
     }
 }
 
 $('#Contents').divcreate();
+
+//
+//
+
+
+
+(function($) {
+        // DOM Ready
+       $(function() {
+           // Binding a click event
+           // From jQuery v.1.7.0 use .on() instead of .bind()
+           $('#video').bind('click', function(e) {
+       var self = $(this) //button
+       , content = $('.content'); 
+       $('element_to_pop_up').bPopup({
+           onOpen: function() {
+               content.html(self.data('bpopup') || '');
+           },
+           onClose: function() {
+               content.empty();
+           }
+       });
+               // Prevents the default action to be triggered. 
+               e.preventDefault();
+               // Triggering bPopup when click event is fired
+               $('#element_to_pop_up').bPopup();
+           });
+       });
+   })(jQuery);
