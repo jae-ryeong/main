ef95ecfdfa2217f69be0b67e9b70bd672230eb61
YongSiHun <tlgns1537@naver.com>
2021-07-29 19:16:58 +0900

    stabilization

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const fs = require('fs');
 const db = require('./db');
 
 app.use(express.static('Front'));
-app.use(bodyParser.urlencoded({extended: false}));
-
 app.get('/', (req, res) => {
     fs.readFile('./Front/html/main.html', (err, data)=>{
     if (err) throw err;
@@ -28,22 +25,22 @@ app.get('/add_data', (req, res) => {
         const link_lnk = Math.random().toString(36).slice(2);
 
         db.create(title_lnk, thumbnail_lnk, link_lnk);
-        res.end(`add_data ${i}`);
+        console.log(`add_data ${i}`);
     }
 });
 
 app.get('/delete_data', (req, res) => {
-    res.end('delete');
+    console.log('delete');
     db.delete_all();
 });
 
 app.get('/find_all_data', (req, res) => {
-    res.end(`DB : ${db}`);
+    console.log(`DB : ${db}`);
     db.find();
 });
 
 app.get('/random_select', (req, res) => {
-    res.end('random_select');
+    console.log('random_select');
     db.random_select(0);
 });
 
