cacc930385b27f3aad264c6548b4969060afb8fe
YongSiHun <tlgns1537@naver.com>
2021-08-03 01:28:25 +0900

    random_select_beta_v1

--- db.js
+++ db.js
@@ -1,60 +1,70 @@
-// mongoose module init
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/testDB', {useNewUrlParser: true})
+const db = mongoose.connection
 
-// testDB setting & using testDB
-mongoose.connect('mongodb://localhost:27017/testDB', {useNewUrlParser: true});
-const db = mongoose.connection;
+db.on('error', () => { console.log('Database Connection failed')})
 
-db.on('error', () => {
-    console.log('Database Connection failed');
-});
+db.once('open', () => { console.log('Database Connected!')})
 
-db.once('open', () => {
-    console.log('Database Connected!');
-});
-
-// Schema create
 const Data_Schema = new mongoose.Schema({
     title : {type : String, required: true},
     thumbnail : {type : String, required: true},
     link : {type : String, required: true},
-});
+})
 
-// defined Schema compile
-const Noodle_Model = mongoose.model('noodle', Data_Schema);
+const Noodle_Model = mongoose.model('noodle', Data_Schema)
 
-// find all data
-exports.find = () => {
+// search data
+find = () => {
     Noodle_Model.find((err, datas) => {
-    console.log('Read all');
+    console.log('Read all')
     
-    if (err) { console.log(err); }
-    else { console.log(datas);}
+    if (err) { throw err }
+    else { for (var i=0;i<datas.length; i++) {
+        console.log(datas[i].title, i, '번째')}
+        }
     });
 }
 
 // data insert
-exports.create = (title_lnk, thumbnail_lnk, link_lnk) => {
-    const add_link = new Noodle_Model({title: title_lnk, thumbnail: thumbnail_lnk, link: link_lnk});
-    add_link.save();
-}
-
-exports.random_select = () => {
-    const Schema_Keys = []
-
-    const get_random_video= (array) => {
-        const index = Math.floor(Math.random() * array.length);
-        return array[index];
-    };
-
-    const key = get_random_video();
+create = (title_lnk, thumbnail_lnk, link_lnk) => {
+    const add_link = new Noodle_Model({title: title_lnk, thumbnail: thumbnail_lnk, link: link_lnk})
+    add_link.save()
 }
 
 // data remove all
-exports.delete_all = () => {
-    console.log("delete!!");
+delete_all = () => {
+    console.log("delete!!")
     Noodle_Model.deleteMany({}, (err) => {
-        if (err) {console.log(err);}
-        else {console.log('delete success');}
+        if (err) { console.log(err) }
+        else { console.log('delete success') }
     });
 }
+
+// data random select
+random_select = () => {
+    const DB_length = Noodle_Model.find((err,datas) => {
+        if (err) { throw err }
+        else {
+            const duple = []
+            for (var i=0; i<9; i++){
+                const random_index = Math.floor(Math.random()*datas.length);
+                if (duple.includes(random_index) == false){
+                    console.log(i+1,'번 영상\n',
+                        'title :', datas[random_index].title, 
+                        '\nthumbnail :', datas[random_index].thumbnail, 
+                        '\nlink :', datas[random_index].link,'\n')
+                    duple.push(random_index)
+                }
+                else {
+                    console.log('\nduple!!\n\n')
+                    i--
+                }
+            }
+            console.log(duple)
+        }
+    })
+
+}
+
+module.exports = {find, create, delete_all, random_select}
