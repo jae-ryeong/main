584bb87f865852e5677b5806c96952f2b4577629
bya2 <byaa1972@gmail.com>
2021-08-14 16:07:11 +0900

    write: 비디오 별 정보 json 형태로 출력, 비디오 프로퍼티 추가

--- src/config/options.js
+++ src/config/options.js
@@ -1,14 +1,16 @@
 module.exports = {
   search: {
     key: process.env.YOUTUBE_TOKEN,
-    q: text,
-    maxResults: 1,
+    part: 'snippet',
+    q: '알파고 햇규',
+    maxResults: 3,
     fields: 'items(id)',
   },
+  
   videos: {
     key: process.env.YOUTUBE_TOKEN,
     part: 'snippet, statistics',
-    id: videoId,
-    fields: 'items(id, snippet(title, description, thumbnails), statistics(viewCount, likeCount, commentCount))',
+    id: 'sk',
+    fields: 'items(id, snippet(channelId, title, description, thumbnails), statistics(viewCount, likeCount, commentCount))',
   },
 }
--- src/service/index.js
+++ src/service/index.js
@@ -15,12 +15,18 @@ const youtube = google.youtube('v3');
 search(youtube).then(items => {
 
   for (const item of items) {
-    const { id } = item;
+    const id = item.id;
     const { kind, videoId, channelId, playlistId } = id;
 
     if (videoId) {
       videos(youtube, videoId).then(item => {
+        const { id, snippet, statistics} = item;
+        const url = `https://www.youtube.com/watch?v=${id}`;
+        const { channelId, title, description, thumbnails } = snippet;
+        const thumbnail = thumbnails.default.url;
+        const { viewCount, likeCount, commentCount } = statistics;
         
+        console.log({ title, thumbnail, url, channelId });
       })
     }
   }
