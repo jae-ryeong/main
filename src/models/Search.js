cdd3edac70674a3cf79ccc91ef752dcdef5fc87a
bya2 <byaa1972@gmail.com>
2021-08-14 17:39:08 +0900

    remove: 작성된 Search 스키마 및 모델 제거

--- src/models/Search.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SearchSchema = new mongoose.Schema({
-  title: { type: String },
-  thumbnail: { type: String },
-  url: { type: String, unique: true }
-});
-
-module.exports = mongoose.model('Search', SearchSchema);
