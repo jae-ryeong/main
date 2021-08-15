0dc5fe127992c3dc68a240722e90c06c3b1751bb
bya2 <byaa1972@gmail.com>
2021-08-15 14:41:45 +0900

    add: Search API의 메소드 옵션 추가

--- src/config/options.js
+++ src/config/options.js
@@ -2,15 +2,21 @@ module.exports = {
   search: {
     key: process.env.YOUTUBE_TOKEN,
     part: 'snippet',
-    q: '알파고 햇규',
+    order: 'date',
     maxResults: 5,
+    q: '된장국',
+    topicId: '호박잎요리',
+    type: 'video',
+    // videoCategoryId: '',
+    videoEmbeddable: 'true', // 웹페이지로 퍼갈 수 있는 동영상만 포함하도록 검색을 제한
+    videoSyndicated: 'true', // youtube.com 외부에서 재생할 수 있는 동영상만 포함 value = 'true' or 'any'
     fields: 'items(id)',
   },
   
   videos: {
     key: process.env.YOUTUBE_TOKEN,
     part: 'snippet, statistics',
-    id: 'sk',
+    id: '',
     fields: 'items(id, snippet(channelId, title, description, thumbnails), statistics(viewCount, likeCount, commentCount))',
   },
 }
