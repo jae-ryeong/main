c160a7512623b1bea1d0f4aec47b4c344dc80b62
bya2 <byaa1972@gmail.com>
2021-08-23 20:17:31 +0900

    데이터가 발견되는 즉시 DB에 저장하지 않고, 안정성을 위해서 배열에 추가한 후 그 배열을 순회하여 DB에 저장

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,39 +3,52 @@ const { google } = require('googleapis');
 
 const db = require('../../db');
 
-const { searchQuery, videoData, channelData } = require('./apiController');
+const { getQueries, searchQuery, videoData, channelData } = require('./apiController');
 const Video = require('../models/Video');
 const Channel = require('../models/Channel');
 
-(() => {
+(async () => {
   const service = google.youtube('v3');
 
-  const q = '라면요리';
-  const topicId = '';
+  const queries = getQueries();
 
-  searchQuery(service, q, topicId).then(items => {
-    items.map(el => {
-      const { kind, videoId, channelId, playlistId } = el.id;
+  const _videos = [], _channels = [];
 
-      if (kind === 'youtube#video'&& videoId) {
-        videoData(service, videoId)
-          .then(async obj => {
-            const videoDoc = new Video(obj);
-            videoDoc.save(err => err ? console.error(err.message) : console.log(`Video's info saved in DB.`));
+  await Promise.all(queries.map(q => {
+    searchQuery(service, q).then(items => {
+      items.map(el => {
+        const { kind, videoId, channelId, playlistId } = el.id;
 
-            if (await Channel.find({ id: channelId })) {
-              return console.log(`Already exists for this channel's info.`);
-            }
-            
-            channelData(service, obj.channelId)
-              .then(obj => {
-                const channelDoc = new Channel(obj);
-                channelDoc.save(err => err ? console.error(err.message) : console.log(`Channel's info saved in DB.`));
-              })
-          });
-      }
-    })
+        if (kind === 'youtube#video'&& videoId) {
+          videoData(service, videoId)
+            .then(async obj => {
+              _videos.push(obj);
+              console.log(`Video's info was successfully added to the array.`);
+
+              if (await Channel.find({ id: channelId })) {
+                return console.log(`Already exists for this channel's info.`);
+              }
+
+              channelData(service, obj.channelId)
+                .then(obj => {
+                  const channelDoc = new Channel(obj);
+                  channelDoc.save(err => err ? console.error(err.message) : console.log(`Channel's info was successfully added to the array.`));
+                })
+            });
+        }
+      })
+    });
+  })).catch(err => console.error(`Error:\n${err.message}`));
+
+  _videos.map(el => {
+    const videoDoc = new Video(el);
+    videoDoc.save(err => err ? console.error(err.message) : console.log(`Video's info saved in DB.`));
   });
+
+  _channels.map(el => {
+    const channelDoc = new Channel(el);
+    channelDoc.save(err => err ? console.error(err.message) : console.log(`Channel's info saved in DB.`));
+  })
 })();
 
 // const getCooking = () => {
