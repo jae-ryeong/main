f55e11f90dc17e093f8c76435cb9251d3b536981
bya2 <byaa1972@gmail.com>
2021-08-24 19:35:07 +0900

    데이터 베이스 구조에 따라 모델 작성

---
+++ models/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  Channel: require('./Channel'),
+  Diet: require('./Diet'),
+  Meal: require('./Meal'),
+  Snack: require('./Snack'),
+  Sports: require('./Sports'),
+  Wellbeing: require('./Wellbeing'),
+  Video: require('./Video'), // using test purpose
+}
