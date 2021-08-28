5a8ba221adabf8c86a22c5afae5e9dc4083708a7
bya2 <byaa1972@gmail.com>
2021-08-28 21:59:00 +0900

    Add Meal, Snack, Sports

---
+++ models/Meal.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model('Video', new mongoose.Schema({
+  query: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  channelId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+  },
+}));
