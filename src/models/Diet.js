19f792856daf3096e19c9aa6910e10e1964a187f
bya2 <byaa1972@gmail.com>
2021-08-31 17:37:44 +0900

    fix: 타이핑 에러 수정(OverwriteModelError)

--- src/models/Diet.js
+++ src/models/Diet.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Video', new mongoose.Schema({
+module.exports = mongoose.model('Diet', new mongoose.Schema({
   query: {
     type: String,
     required: true,
--- src/models/Meal.js
+++ src/models/Meal.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Video', new mongoose.Schema({
+module.exports = mongoose.model('Meal', new mongoose.Schema({
   query: {
     type: String,
     required: true,
