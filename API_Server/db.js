6492d9865cf6c0c54dc70e42f1ac2f5addbcb791
bya2 <byaa1972@gmail.com>
2021-09-13 22:53:13 +0900

    write: API DB connnect

---
+++ API_Server/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// 데이터 베이스 연결
+mongoose.connect(process.env.MONGODB_CONNECTION || 'mongodb://localhost:27017/mohaemookji', { useNewUrlParser: true })
+
+// 이벤트 이미터 생성
+const db = mongoose.connection
+
+// 에러, 연결 시 처리
+db.on('error', err => console.error(`Error on connection:\n${err.message}`));
+db.once('open', () => console.log('Database connected.'));
+
+module.exports = db;
