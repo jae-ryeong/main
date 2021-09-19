9bc4582b4f7b289d6666bef1fed2cf73010954bf
YongSiHun <tlgns1537@naver.com>
2021-09-19 22:26:30 +0900

    DB_test_in_compose

--- main/main_server.js
+++ main/main_server.js
@@ -4,7 +4,7 @@ const app = express()
 const fs = require('fs')
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_CONNECTION || 'mongodb://10.0.0.13:24', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_CONNECTION || 'mongodb://172.20.0.2:27017', { useNewUrlParser: true })
 const db = mongoose.connection
 
 db.on('error', err => console.error(`Error on connection:\n${err.message}`));
