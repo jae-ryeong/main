8ae130f3ecacae3c1221843e2774d93b838a3ca7
YongSiHun <tlgns1537@naver.com>
2021-09-20 03:18:21 +0900

    db-server-linking

--- main/main_server.js
+++ main/main_server.js
@@ -4,7 +4,7 @@ const app = express()
 const fs = require('fs')
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_CONNECTION || 'mongodb://mh-db-server:27017', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_CONNECTION || 'mongodb://db:27017', { useNewUrlParser: true })
 const db = mongoose.connection
 
 db.on('error', err => console.error(`Error on connection:\n${err.message}`));
