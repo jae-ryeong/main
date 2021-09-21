9d4ce30bea1096d1779a3386b6fee911eee80539
YongSiHun <tlgns1537@naver.com>
2021-09-21 21:33:39 +0900

    DB-warning-erase

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // 데이터 베이스 연결
-mongoose.connect(process.env.MONGODB_CONNECTION || 'mongodb://db:27106/mohaemookji', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_CONNECTION || 'mongodb://db:27261/mohaemookji', { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 이벤트 이미터 생성
 const db = mongoose.connection
