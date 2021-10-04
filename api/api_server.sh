ddd1cf7a0a6b094589c2e936d50a49d18766a102
YongSiHun <tlgns1537@naver.com>
2021-10-04 21:16:21 +0900

    debugging

--- api/api_server.sh
+++ api/api_server.sh
@@ -2,4 +2,5 @@
 rm -Rf /usr/mohsemookji/main /usr/mohaemookji/TEST-main-page
 killall -s KILL node
 sleep 5s
-node /usr/mohaemookji/api/youtube -DFOREGROUND
+cd /usr/mohaemookji/api
+node youtube -DFOREGROUND
--- main/main_server.sh
+++ main/main_server.sh
@@ -2,4 +2,5 @@
 rm -Rf /usr/mohsemookji/api /usr/mohaemookji/TEST-main-page
 killall -s KILL node
 sleep 5s
-node /usr/mohaemookji/main/main_server.js
+cd /usr/mohaemookji/main
+node main_server.js -DFOREGROUND
