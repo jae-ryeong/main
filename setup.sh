a1b9e270ddbd097bde0b7560cd9f7c8ec9c5f7d4
YongSiHun <tlgns1537@naver.com>
2021-10-15 21:32:03 +0900

    login shell delete

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemookji/main
-bash /var/lib/jenkins/workspace/login.sh
 docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
