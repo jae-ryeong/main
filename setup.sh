46ece116bad5a2ca2d8576d1ea41e505c5c68281
YongSiHun <tlgns1537@naver.com>
2021-10-23 17:37:49 +0900

    orche

--- setup.sh
+++ setup.sh
@@ -3,6 +3,4 @@ sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemoo
 docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
-docker service update mh-stack_main --replicas 5
-docker service update mh-stack_api --replicas 5
-docker service update mh-stack_db
+docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack --orchestratory swarm 
