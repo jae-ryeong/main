909c00f8579212deb9080bf04d36f24331170f49
YongSiHun <tlgns1537@naver.com>
2021-10-23 19:38:25 +0900

    latest update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemookji/main
 bash /var/lib/jenkins/workspace/mh-login.sh
-docker build --tag 2mukee/mh_main_deploy:1.0 /var/lib/jenkins/workspace/mohaemookji/main
-docker build --tag 2mukee/mh_api_deploy:1.0 /var/lib/jenkins/workspace/mohaemookji/api
-docker build --tag 2mukee/mh_db_deploy:1.0 /var/lib/jenkins/workspace/mohaemookji
-# docker push 2mukee/mh_main_deploy
-# docker push 2mukee/mh_api_deploy
-# docker push 2mukee/mh_db_deploy
+docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
+docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
+docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
+docker push 2mukee/mh_main_deploy
+docker push 2mukee/mh_api_deploy
+docker push 2mukee/mh_db_deploy
 docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack
