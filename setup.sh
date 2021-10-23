0874fd7a536d7c98436a0b62c78e49c9c57ce00a
YongSiHun <tlgns1537@naver.com>
2021-10-23 17:27:57 +0900

    docker service update add

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ bash /var/lib/jenkins/workspace/mh-login.sh
 docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
-docker stack deploy --with-registry-auth -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack 
+docker service update mh-stack_main
+docker service update mh-stack_api
+docker service update mh-stack_db
