75e44764ad0ec6c9b301fbe03c94ee271ee9c40e
YongSiHun <tlgns1537@naver.com>
2021-10-23 16:46:17 +0900

    update for high availability

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemoo
 docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
-docker stack stop mh-stack
+docker stack rm mh-stack
 sleep 15s
 docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack
