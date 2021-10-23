33b6feaf7812a21f9ac4768799a6be84dcdfc86c
YongSiHun <tlgns1537@naver.com>
2021-10-23 18:17:33 +0900

    deploy image push add

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemookji/main
-# bash /var/lib/jenkins/workspace/mh-login.sh
-docker build --tag 2mukee/mh_main_deploy:latest /var/lib/jenkins/workspace/mohaemookji/main
-docker build --tag 2mukee/mh_api_deploy:latest /var/lib/jenkins/workspace/mohaemookji/api
-docker build --tag 2mukee/mh_db_deploy:latest /var/lib/jenkins/workspace/mohaemookji
-docker stack deploy --resolve-image -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack
+bash /var/lib/jenkins/workspace/mh-login.sh
+docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
+docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
+docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
+docker push 2mukee/mh_main_deploy
+docker push 2mukee/mh_api_deploy
+docker push 2mukee/mh_db_deploy
+docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack
