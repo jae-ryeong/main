6dd4fdbb132e0e8021faf2e86d9371185d4edd33
YongSiHun <tlgns1537@naver.com>
2021-10-23 18:10:46 +0900

    resolve image option add

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemoo
 docker build --tag 2mukee/mh_main_deploy:latest /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy:latest /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy:latest /var/lib/jenkins/workspace/mohaemookji
-docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack --resolve-image
+docker stack deploy  --resolve-image -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack
