e5ccb5fa86eb088e95d7ea67de69120676b39386
YongSiHun <tlgns1537@naver.com>
2021-10-23 18:04:46 +0900

    deploy test for stack update

--- setup.sh
+++ setup.sh
@@ -4,6 +4,4 @@ sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemoo
 docker build --tag 2mukee/mh_main_deploy:latest /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy:latest /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy:latest /var/lib/jenkins/workspace/mohaemookji
-docker service update --image 2mukee/mh_main_deploy mh-stack_main
-docker service update --image 2mukee/mh_api_deploy mh-stack_api
-docker service update --image 2mukee/mh_db_deploy mh-stack_db
+docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack
