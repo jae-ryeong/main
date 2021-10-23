fa237adadd56e755fb4aea22f737910ca44884ee
YongSiHun <tlgns1537@naver.com>
2021-10-23 17:24:32 +0900

    test for high availability

--- setup.sh
+++ setup.sh
@@ -4,6 +4,4 @@ bash /var/lib/jenkins/workspace/mh-login.sh
 docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
-docker stack rm mh-stack
-sleep 15s
 docker stack deploy --with-registry-auth -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack 
