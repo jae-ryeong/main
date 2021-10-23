1f83a6143c70aa00b96920ecd3320e655ee60284
YongSiHun <tlgns1537@naver.com>
2021-10-23 17:33:25 +0900

    replicas option add

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemookji/main
-bash /var/lib/jenkins/workspace/mh-login.sh
 docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
-docker service update mh-stack_main
-docker service update mh-stack_api
+docker service update mh-stack_main --replicas 5
+docker service update mh-stack_api --replicas 5
 docker service update mh-stack_db
