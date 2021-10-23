f9040c304c134c59a6e11ae98eb5b1f7a3bf8954
YongSiHun <tlgns1537@naver.com>
2021-10-23 17:45:36 +0900

    docker push for resolve deploy problem

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ bash /var/lib/jenkins/workspace/mh-login.sh
 docker build --tag 2mukee/mh_main_deploy:latest /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy:latest /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy:latest /var/lib/jenkins/workspace/mohaemookji
-docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack --orchestrator swarm --with-registry-auth
+docker push 2mukee/mh_main_deploy:latest
+docker push 2mukee/mh_api_deploy:latest
+docker push 2mukee/mh_db_deploy:latest
+docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack --with-registry-auth
