ee6bb1cecc03fe49eaf692d117fa2b3e4a980824
YongSiHun <tlgns1537@naver.com>
2021-10-23 16:56:33 +0900

    deploy auth problem solve

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sudo cp -r /var/lib/jenkins/workspace/mh-ssl /var/lib/jenkins/workspace/mohaemoo
 docker build --tag 2mukee/mh_main_deploy /var/lib/jenkins/workspace/mohaemookji/main
 docker build --tag 2mukee/mh_api_deploy /var/lib/jenkins/workspace/mohaemookji/api
 docker build --tag 2mukee/mh_db_deploy /var/lib/jenkins/workspace/mohaemookji
-docker stack deploy -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack
+docker stack deploy --with-registry-auth -c /var/lib/jenkins/workspace/mohaemookji/mh-compose.yml mh-stack 
