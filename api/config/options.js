11eddbd7e2ee82e2e42ecdc2d0a0752d2d97a9ec
bya2 <byaa1972@gmail.com>
2021-11-14 16:04:30 +0900

    [이진형] Change variable name, fix line and blank, remove videos statistics

--- api/config/options.js
+++ api/config/options.js
@@ -1,40 +1,33 @@
-const fnExtractToken = () => {
-  const arrTokens = process.env.YOUTUBE_TOKEN.split(' ');
-  return arrTokens[(new Date().getDate()) % arrTokens.length];
+const KEY = require('../jobs/key');
+
+const apiOpts = {
+  search: {
+    key            : KEY,
+    part           : 'id',
+    order          : 'date',
+    maxResults     : 3,
+    type           : 'video',
+    videoEmbeddable: 'true',
+    videoSyndicated: 'true',
+    fields         : 'item(id)',
+  },
+  videos: {
+    key            : KEY,
+    part           : 'snippet', // +statistics
+    fields         : 'items(id, snippet(channelId, title, thumbnails))'
+  },
+  channels: {
+    key            : KEY,
+    part           : 'snippet, statistics',
+    fields         : 'items(id, snippet(title, thumbnails), statistics(viewCount, commentCount, subscriberCount, videoCount))',
+  },
+  searchChannels: {
+    key            : KEY,
+    part           : 'id',
+    categoryId     : '',
+    maxResults     : 3,
+    fields         : 'items(id)',
+  }
 };
 
-const YOUTUBE_TOKEN = fnExtractToken();
-
-module.exports = {
-  search: {
-    key: YOUTUBE_TOKEN,
-    part: 'id',
-    order: 'date',
-    maxResults: 3,
-    type: 'video',
-    videoEmbeddable: 'true', // 저작권 관련 문제: 웹페이지로 퍼갈 수 있는 동영상만 포함하도록 검색을 제한
-    videoSyndicated: 'true', // 저작권 관련 문제: youtube.com 외부에서 재생할 수 있는 동영상만 포함 value = 'true' or 'any'
-    fields: 'items(id)',
-  },
-  
-  videos: {
-    key: YOUTUBE_TOKEN,
-    part: 'snippet, statistics',
-    fields: 'items(id, snippet(channelId, title, thumbnails))',
-    // fields: 'items(id, snippet(channelId, title, description, thumbnails), statistics(viewCount, likeCount, commentCount))',
-  },
-
-  channels: {
-    key: YOUTUBE_TOKEN,
-    part: 'snippet, statistics',
-    fields: 'items(id, snippet(title, thumbnails), statistics(viewCount, commentCount, subscriberCount, videoCount))',
-  },
-
-  searchChannels: {
-    key: YOUTUBE_TOKEN,
-    part: 'id',
-    categoryId: '',
-    maxResults: 3,
-    fields: 'items(id)',
-  },
-}
+module.exports.apiOpts = apiOpts;
