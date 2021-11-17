2ecd9426d37dae7fdabb322d04ffae682cf60336
YongSiHun <tlgns1537@naver.com>
2021-11-17 22:06:35 +0900

    add require(fs) on main server

--- main/index.js
+++ main/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const fs = require('fs');
 
 const port = 17260;
 
